11b626478f0b9dc5dbb893b34956f3d880918e68
Leonardo <leonardo11emn@gmail.com>
2022-05-06 17:06:03 -0500

    Detalle de las vacantes

--- src/components/JobCard..jsx
+++ src/components/JobCard..jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import logo from "../imports/img/logo.png"
 
 function JobCard() {
   return (
-    <div className="p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 m-2 grid grid-cols-3 cursor-pointer" >
+    <Link to={"/vacante"} className="p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 m-2 grid grid-cols-3 cursor-pointer" >
         <img src={logo} alt="Company logo" />
         <section>
             <article><h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Job Title</h5></article>
@@ -14,7 +15,7 @@ function JobCard() {
             <article>Remote</article>
             <article>Salary</article>
         </section>
-    </div>
+    </Link>
   )
 }
 
--- src/pages/DetalleEmpleo.jsx
+++ src/pages/DetalleEmpleo.jsx
@@ -1,84 +1,134 @@
-import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  solid,
+  regular,
+  brands,
+} from "@fortawesome/fontawesome-svg-core/import.macro";
+import React, { useState } from "react";
 import styled from "styled-components";
 import logo from "../imports/img/logo.png";
+import { useNavigate } from "react-router-dom";
 
-export const DetalleEmpleo = () => {
+function ReadMore({ children = 100 }) {
+  const text = children;
+
+  const [isShow, setIsShowLess] = useState(true);
+  const result = isShow ? text.slice(0, 100) : text;
+
+  function toggleIsShow() {
+    setIsShowLess(!isShow);
+  }
+
   return (
     <>
-      <Card>
-        <Topbar>
-          <span>⬅</span>
-          <span>❤</span>
-        </Topbar>
-        <Img src={logo} alt={"logo"} />
-        <Title>Nombre de la vacante</Title>
-        <Subtitle>Nombre de la empresa</Subtitle>
-        <span>Publicada: 10 Jul 2022</span>
-      </Card>
-      <Card>
-        <BoxDetalles>
-          <div>
-            <Subtitle>Aplicar antes de</Subtitle>
-            <span> 20 Oct 2022</span>
-          </div>
-          <div>
-            <Subtitle>Trabjo</Subtitle>
-            <span>Hacer algo productivo</span>
-          </div>
-          <div>
-            <Subtitle>Rango salarial</Subtitle>
-            <span> $1000 - $2000</span>
-          </div>
-          <div>
-            <Subtitle>Ubicacion</Subtitle>
-            <span> 20 Oct 2022</span>
-          </div>
-        </BoxDetalles>
-      </Card>
-      <Card>
-        <Subtitle>Descripcion del trabajo</Subtitle>
-        <Text>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus,
-          neque. Quia nam optio cumque eius eum fugit at repellendus atque,
-          porro sint ab perferendis minus consequuntur dolorem fugiat fuga
-          suscipit. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Exercitationem consequatur quis animi molestias, itaque dolores
-          veritatis similique? Nam fugit saepe id unde minima, dolores
-          voluptates error, ex laborum omnis maiores.
-        </Text>
-      </Card>
-      <Card>
-        <Subtitle>Descripcion del trabajo</Subtitle>
-        <Text>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus,
-          neque. Quia nam optio cumque eius eum fugit at repellendus atque,
-          porro sint ab perferendis minus consequuntur dolorem fugiat fuga
-          suscipit. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Exercitationem consequatur quis animi molestias, itaque dolores
-          veritatis similique? Nam fugit saepe id unde minima, dolores
-          voluptates error, ex laborum omnis maiores.
-        </Text>
-      </Card>
+      {result}
+      <Text className="btn btn-link" onClick={toggleIsShow}>
+        {isShow ? "Leer mas" : "Leer menos"}
+        <FontAwesomeIcon style={{padding:"0px 5px"}} icon={isShow ? solid("caret-down") : solid("caret-up")} />
+      </Text>
+    </>
+  );
+}
+
+export const DetalleEmpleo = () => {
+  const back = useNavigate();
+
+  const [red, setRed] = useState(false);
+  return (
+    <>
+      {" "}
+      <Topbar>
+        <Icon icon={solid("arrow-left")} onClick={() => back(-1)} />
+        <Icon
+          icon={red ? solid("heart") : regular("heart")}
+          onClick={() => {
+            setRed(!red);
+          }}
+          red={red}
+        />
+      </Topbar>
+      <CardContainer>
+        <Card>
+          <Img src={logo} alt={"logo"} />
+          <Title>Nombre de la vacante</Title>
+          <Subtitle>Nombre de la empresa</Subtitle>
+          <span>Publicada: 10 Jul 2022</span>
+        </Card>
+        <Card>
+          <BoxDetalles>
+            <div>
+              <Subtitle>Aplicar antes de</Subtitle>
+              <span> 20 Oct 2022</span>
+            </div>
+            <div>
+              <Subtitle>Trabjo</Subtitle>
+              <span>Hacer algo productivo</span>
+            </div>
+            <div>
+              <Subtitle>Rango salarial</Subtitle>
+              <span> $1000 - $2000</span>
+            </div>
+            <div>
+              <Subtitle>Ubicacion</Subtitle>
+              <span> 20 Oct 2022</span>
+            </div>
+          </BoxDetalles>
+        </Card>
+        <Card>
+          <Subtitle>Descripcion del trabajo</Subtitle>
+          <ReadMore>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus,
+            neque. Quia nam optio cumque eius eum fugit at repellendus atque,
+            porro sint ab perferendis minus consequuntur dolorem fugiat fuga
+            suscipit. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Exercitationem consequatur quis animi molestias, itaque dolores
+            veritatis similique? Nam fugit saepe id unde minima, dolores
+            voluptates error, ex laborum omnis maiores.
+          </ReadMore>
+        </Card>
+        <Card>
+          <Subtitle>Descripcion del trabajo</Subtitle>
+          <ReadMore>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus,
+            neque. Quia nam optio cumque eius eum fugit at repellendus atque,
+            porro sint ab perferendis minus consequuntur dolorem fugiat fuga
+            suscipit. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Exercitationem consequatur quis animi molestias, itaque dolores
+            veritatis similique? Nam fugit saepe id unde minima, dolores
+            voluptates error, ex laborum omnis maiores.
+          </ReadMore>
+        </Card>
+      </CardContainer>
+      <div style={{ height: "50px" }} />
       <BotonArea>
         <Button>Aplicar</Button>
       </BotonArea>
     </>
   );
 };
+const CardContainer = styled.div`
+  margin: 0 auto;
+  display: grid;
+  grid-gap: 10px;
+  grid-template-columns: repeat(auto-fill, minmax(20rem, 1fr));
+`;
 const Card = styled.div`
   padding: 25px 30px;
   background-color: white;
-  margin: 5px 0px;
-  max-width: 700px;
+  margin: 0px 0px 5px 0px;
+  border-radius: 4px;
+
   & span {
     font-size: 12px;
     font-weight: 400;
   }
 `;
 const Topbar = styled.div`
+  height: 50px;
+  padding: 5px 25px;
+  background-color: white;
   display: flex;
   justify-content: space-between;
-  margin-bottom: 30px;
 `;
 const Img = styled.img`
   width: 50px;
@@ -101,18 +151,32 @@ const BoxDetalles = styled.div`
   }
 `;
 const Text = styled.p`
-  text-overflow: ellipsis;
+  color: #1c58f2;
+  font-weight: 600;
 `;
 const Button = styled.button`
-  padding: 5px;
+  padding: 10px 50px;
+  font-size: 16px;
+  background-color: transparent;
+  cursor: pointer;
+  border-radius: 4px;
+  border: 2px solid #313131;
+  background: #1c58f2;
 `;
 const BotonArea = styled.div`
-background-color: green;
-width: 100%;
-height: 50px;
+  margin: 5px;
+  color: white;
+  width: 100%;
+  height: 50px;
   position: fixed;
   bottom: 0;
   display: flex;
   align-items: center;
   justify-content: center;
+`;
+const Icon = styled(FontAwesomeIcon)`
+  color: ${(props) => (props.red ? "red" : "black")};
+  align-self: center;
+  font-size: 20px;
+  cursor: pointer;
 `;
